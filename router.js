347c28be301d131fd15cb9a85c695d799d7ded0f
Axel <kurtastraube@gmail.com>
2015-06-08 17:24:35 -0700

    Add initial files and basic page layout.

---
+++ router.js
@@ -0,0 +1,8 @@
+Blog.Router.map(function() {
+	this.resource('posts', {path: '/'});
+	this.resource('about');
+	this.resource('contact', function() {
+		this.resource('phone');
+		this.resource('email');
+	});
+});
