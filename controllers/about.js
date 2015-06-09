75e213ffb7f359a5c006b0eb82d00047a004fd53
Axel <kurtastraube@gmail.com>
2015-06-09 09:55:43 -0700

    Add img, update page buttons.

--- controllers/about.js
+++ controllers/about.js
@@ -1,1 +1,14 @@
-Blog.AboutController
+Blog.AboutController = Ember.Controller.extend({
+	isPictureShowing: false,	
+	actions: {
+		buttonAction: function() {
+			alert('TEST');
+		},
+		showPicture: function() {
+			this.set('isPictureShowing', true);
+		},
+		hidePicture: function() {
+			this.set('isPictureShowing', false);
+		}
+	} 
+});
